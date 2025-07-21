ed11e238509c69f361639fce6f8dfaf77cbc96b1
Brian <brian@brian@gmail.com>
2025-07-21 23:24:07 +0300

    submission

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css'  // ← Changed this line
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
